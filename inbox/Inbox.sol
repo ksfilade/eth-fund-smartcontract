afb695d380b58c295bfcda154842ad54e771f286
kiril <ksfilade@yahoo.com>
2019-11-22 17:19:42 +0100

    added to field on event

--- inbox/Inbox.sol
+++ inbox/Inbox.sol
@@ -2,11 +2,19 @@ pragma solidity ^0.4.17;
 
 contract Inbox {
     string public message;
+     event Deposit(
+        address indexed _from,
+        uint _value,
+        address indexed _to
+    );
+
 
     constructor (string initialMessage) public {
         message = initialMessage;
     }
     function send(address fundAddress) public payable {
-        fundAddress.transfer(msg.value);
+      fundAddress.transfer(msg.value);
+      emit Deposit(msg.sender, msg.value, fundAddress);
+
     }
 }
